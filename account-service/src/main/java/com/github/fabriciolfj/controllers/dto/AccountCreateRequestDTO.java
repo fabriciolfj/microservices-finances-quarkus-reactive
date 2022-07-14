813150fcfcb38cc7995a6a224a267b2f0f49ebd1
fabricio jacob <fabricio.jacob@outlook.com>
2022-07-13 21:56:39 -0300

    add

--- account-service/src/main/java/com/github/fabriciolfj/controllers/dto/AccountCreateRequestDTO.java
+++ account-service/src/main/java/com/github/fabriciolfj/controllers/dto/AccountCreateRequestDTO.java
@@ -2,10 +2,12 @@ package com.github.fabriciolfj.controllers.dto;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Data
 public class AccountCreateRequestDTO {
 
+    @NotNull(message = "Balance not found")
     private BigDecimal entrance;
 }
